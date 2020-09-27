86048cab6afaea2b2d4fb00753dbd85593d6877f
John Allwine <jallwine86@gmail.com>
2020-09-27 13:04:04 -0600

    relative build

---
+++ precache-manifest.9d069694f5ef130384931843c628198a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2028d3b759663e646bfabbcb6dc084f5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "681411aa470e423f8ed1",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6f94da342badd4506dde",
+    "url": "./static/js/2.fc50668e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.fc50668e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681411aa470e423f8ed1",
+    "url": "./static/js/main.5250b6bf.chunk.js"
+  },
+  {
+    "revision": "bdcf0ca709f396ce8c37",
+    "url": "./static/js/runtime-main.498c4610.js"
+  }
+]);
