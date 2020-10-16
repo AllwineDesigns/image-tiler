ced9e8afbcf36248cdc46ced8a454afb249a1d19
John Allwine <jallwine86@gmail.com>
2020-10-16 16:14:27 -0600

    tile using filled rects with pattern

--- src/App.js
+++ src/App.js
@@ -33,10 +33,10 @@ function App() {
     const filename = e.target.files[0].name;
     const reader = new FileReader();
     reader.onload = (e) => {
+      const imageAsDataURL = e.target.result;
       const img = document.createElement('img');
       img.onload = () => {
         console.log(img.width, img.height);
-        const canvas = document.createElement("canvas");
 
         if(!width && height) {
           width = img.width/img.height*height;
@@ -46,33 +46,28 @@ function App() {
           height = img.height/img.width*width;
         }
 
-        if(width) {
-          canvas.width = width/cols;
-        } else {
-          canvas.width = img.width/cols;
+        if(!width && !height) {
+          width = img.width;
+          height = img.height
         }
 
-        if(height) {
-          canvas.height = height/rows;
-        } else {
-          canvas.height = img.height/rows;
-        }
-
-        const context = canvas.getContext('2d');
         const tileWidth = img.width/cols;
         const tileHeight = img.height/rows;
 
         const images = [];
         for(let r = 0; r < rows; r++) {
           for(let c = 0; c < cols; c++) {
-            context.clearRect(0,0,canvas.width, canvas.height);
-            context.drawImage(img, c*tileWidth, r*tileHeight, tileWidth, tileHeight, 0, 0, canvas.width, canvas.height);
-            images.push(<g key={`${r},${c}`}><image imageRendering="optimizeSpeed" x={c*canvas.width} y={r*canvas.height} width={canvas.width} height={canvas.height} xlinkHref={canvas.toDataURL()}/></g>);
+            images.push(<g key={`${r},${c}`}><rect x={c*tileWidth} y={r*tileHeight} width={tileWidth} height={tileHeight} fill="url(#Image)" stroke="none"/></g>);
           }
         }
 
         const d = document.createElement("div");
         ReactDOM.render(<svg width={width ? width/dpi + "in" : img.width/dpi + "in"} height={height ? height/dpi + "in" : img.height/dpi + "in" } viewBox={"0 0 " + (width ? width : img.width) + " " + (height ? height : img.height)} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+          <defs>
+            <pattern id="Image" x="0" y="0" width={img.width} height={img.height} patternUnits="userSpaceOnUse">
+              <image xlinkHref={imageAsDataURL} x="0" y="0" width={img.width} height={img.height}/>
+            </pattern>
+          </defs>
           {images}
         </svg>, d);
         const a = document.createElement('a');
@@ -84,7 +79,7 @@ function App() {
 
         fileRef.current.value = "";
       };
-      img.src = e.target.result;
+      img.src = imageAsDataURL;
     };
     reader.readAsDataURL(e.target.files[0]);
   };
