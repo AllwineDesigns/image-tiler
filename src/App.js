28d2da4bab5d5d1d38573f8a6bdc7d9005dd6345
John Allwine <jallwine86@gmail.com>
2020-11-08 22:06:01 -0700

    added wrap tiles in SVG option as edge pixel interpolation seems to give better behavior on Glowforge changed image-rendering to pixelated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ReactDOM from 'react-dom';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -11,8 +12,27 @@ const useStyles = makeStyles((theme) => ({
   },
   button: {
     margin: theme.spacing(1)
+  },
+  checkboxLabel: {
+    lineHeight: "40px",
+    verticalAlign: "middle"
   }
 }));
+
+const renderToString = (reactTree) => {
+  const d = document.createElement("div");
+  ReactDOM.render(reactTree, d);
+  return d.innerHTML;
+};
+
+const downloadString = (str, { type, filename }) => {
+  const a = document.createElement('a');
+  const data = new Blob([ str ], { type });
+  const url = window.URL.createObjectURL(data);
+  a.download = filename;
+  a.href = url;
+  a.click();
+};
 
 function App() {
   const classes = useStyles();
@@ -22,6 +42,7 @@ function App() {
   const rowsRef = useRef(null);
   const colsRef = useRef(null);
   const fileRef = useRef(null);
+  const [ wrapTiles, setWrapTiles ] = useState(true);
 
   const handleChange = (e) => {
     const rows = parseInt(rowsRef.current.value) || 2;
@@ -29,6 +50,7 @@ function App() {
     const dpi = parseInt(dpiRef.current.value) || 225;
     let width = widthRef.current.value === "auto" ? undefined : parseInt(widthRef.current.value);
     let height = heightRef.current.value === "auto" ? undefined : parseInt(heightRef.current.value);
+
 
     const filename = e.target.files[0].name;
     const reader = new FileReader();
@@ -67,20 +89,33 @@ function App() {
           for(let c = 0; c < cols; c++) {
             context.clearRect(0,0,canvas.width, canvas.height);
             context.drawImage(img, c*tileWidth, r*tileHeight, tileWidth, tileHeight, 0, 0, canvas.width, canvas.height);
-            images.push(<g key={`${r},${c}`}><image imageRendering="optimizeSpeed" x={c*canvas.width} y={r*canvas.height} width={canvas.width} height={canvas.height} xlinkHref={canvas.toDataURL()}/></g>);
+
+            if(wrapTiles) {
+              const tileSVGStr = renderToString(
+                <svg width={canvas.width} height={canvas.height} viewBox={"0 0 " + canvas.width + " " + canvas.height} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                  <image imageRendering="pixelated" x={0} y={0} width={canvas.width} height={canvas.height} xlinkHref={canvas.toDataURL()}/>
+                </svg>);
+
+  //            const svgURL = "data:image/svg+xml," + encodeURIComponent(tileSVGStr);
+              const svgURL = "data:image/svg+xml;base64," + btoa(tileSVGStr);
+              images.push(<image key={`${r},${c}`} imageRendering="pixelated" x={c*canvas.width} y={r*canvas.height} width={canvas.width} height={canvas.height} xlinkHref={svgURL}/>);
+            } else {
+              images.push(<image key={`${r},${c}`} imageRendering="pixelated" x={c*canvas.width} y={r*canvas.height} width={canvas.width} height={canvas.height} xlinkHref={canvas.toDataURL()}/>);
+            }
           }
         }
 
-        const d = document.createElement("div");
-        ReactDOM.render(<svg width={width ? width/dpi + "in" : img.width/dpi + "in"} height={height ? height/dpi + "in" : img.height/dpi + "in" } viewBox={"0 0 " + (width ? width : img.width) + " " + (height ? height : img.height)} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        const svgStr = renderToString(<svg width={width ? width/dpi + "in" : img.width/dpi + "in"} height={height ? height/dpi + "in" : img.height/dpi + "in" } viewBox={"0 0 " + (width ? width : img.width) + " " + (height ? height : img.height)} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
           {images}
-        </svg>, d);
-        const a = document.createElement('a');
-        const data = new Blob([ d.innerHTML ], { type: 'image/svg+xml' });
-        const url = window.URL.createObjectURL(data);
-        a.download = filename + ".svg";
-        a.href = url;
-        a.click();
+        </svg>);
+
+        downloadString(svgStr, { type: 'image/svg+xml', filename: filename + ".svg" });
+
+//        const svgURL = "data:image/svg+xml;base64," + btoa(svgStr);
+//        const svgStr2 = renderToString(<svg width={width ? width/dpi + "in" : img.width/dpi + "in"} height={height ? height/dpi + "in" : img.height/dpi + "in" } viewBox={"0 0 " + (width ? width : img.width) + " " + (height ? height : img.height)} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+//          <image x={0} y={0} width={(width ? width : img.width)} height={(height ? height : img.height)} xlinkHref={svgURL}/>
+//        </svg>);
+//        downloadString(svgStr2, { type: 'image/svg+xml', filename: filename + ".svg" });
 
         fileRef.current.value = "";
       };
@@ -95,6 +130,7 @@ function App() {
       <Grid item xs={6}>Height:</Grid><Grid item xs={6}><TextField inputRef={heightRef} defaultValue="auto" fullWidth={true}/></Grid>
       <Grid item xs={6}>Rows:</Grid><Grid item xs={6}><TextField inputRef={rowsRef} defaultValue="2" fullWidth={true}/></Grid>
       <Grid item xs={6}>Cols:</Grid><Grid item xs={6}><TextField inputRef={colsRef} defaultValue="2" fullWidth={true}/></Grid>
+      <Grid item xs={6} classes={ { root: classes.checkboxLabel }}>Wrap Tiles In SVG:</Grid><Grid item xs={6}><Checkbox checked={wrapTiles} onChange={(e) => setWrapTiles(e.target.checked)}/></Grid>
       <Grid item xs={12}><Button fullWidth={true} classes={{ root: classes.button }} variant="contained" component="label">Convert File
         <input type="file" style={{ display: "none" }} ref={fileRef} onChange={handleChange}/>
       </Button></Grid>
