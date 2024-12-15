53f119f4a1d622cde960e3dd2e379f2962f47a06
John Allwine <john@pocketnc.com>
2024-12-15 09:07:47 -0700

    npm run build, copied build dir to docs

---
+++ docs/precache-manifest.bf98ed7a043e569d0fe27fdfd6a9d5a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d262d40dbd1d0f7e352bf4856b563a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8aba2ddc68fba8842eba",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "464ddfb803452dac61a1",
+    "url": "./static/js/2.50d09d04.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.50d09d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aba2ddc68fba8842eba",
+    "url": "./static/js/main.4ff12422.chunk.js"
+  },
+  {
+    "revision": "bdcf0ca709f396ce8c37",
+    "url": "./static/js/runtime-main.498c4610.js"
+  }
+]);
